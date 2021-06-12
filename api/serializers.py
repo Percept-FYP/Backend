fb0f009bfecf68416e930020e46a18f9773b5d2c
Zaibanaman1 <68388643+Zaibanaman1@users.noreply.github.com>
2021-06-12 21:27:46 +0530

    register
    Register user

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
-from .models import attendance, info, teacher, Class, attendance, Student, Subject
+from .models import attendance, info, teacher, Class, attendance, Student, Subject, User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
 
 
 class StudentSerializer(serializers.ModelSerializer):
@@ -27,20 +30,18 @@ class AttendanceSerializer(serializers.ModelSerializer):
 class ClassSerializer(serializers.ModelSerializer):
     attendance_set = AttendanceSerializer(many=True)
 
-
     class Meta:
         model = Class
         fields = ['subject', "id", 'attendance_set']
 
     def create(self, validated_data):
-            attendances_data = validated_data.pop('attendance_set')
-            print("thi s",attendances_data)
-            ClASS = Class.objects.create(**validated_data)
-            for attendance_data in attendances_data:
-                print("thi s",attendance_data)
-                attendance.objects.create(Class=ClASS, **attendance_data)
-            return Class       
-
+        attendances_data = validated_data.pop('attendance_set')
+        print("thi s", attendances_data)
+        ClASS = Class.objects.create(**validated_data)
+        for attendance_data in attendances_data:
+            print("thi s", attendance_data)
+            attendance.objects.create(Class=ClASS, **attendance_data)
+        return Class
 
 
 class infoSerializer(serializers.ModelSerializer):
@@ -59,6 +60,14 @@ class TeacherSerializer(serializers.ModelSerializer):
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super(MyTokenObtainPairSerializer, self).validate(attrs)
+        data.update({'user': self.user.username})
+        data.update({'id': self.user.id})
+        data.update({'role': self.user.role})
+
+        return data
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
@@ -72,3 +81,57 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+    token_obtain_pair = TokenObtainPairView.as_view()
+
+#####registr#####
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2',
+                  'email', 'first_name', 'last_name', 'role')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            role=validated_data['role']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+# {
+# "username":"xai",
+# "password":"1234",
+# "password2":"1234",
+# "email":"xaib@gmail.com",
+# "first_name":"syed",
+# "last_name":"zaiban",
+# "role":"student",
+# }
--- api/views.py
+++ api/views.py
@@ -4,7 +4,7 @@ from api.recognize_faces_image import func
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from api.serializers import AttendanceSerializer, infoSerializer, ClassSerializer
+from api.serializers import AttendanceSerializer, infoSerializer, ClassSerializer, RegisterSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from api.models import *
@@ -38,6 +38,17 @@ import datetime
 
 #     serializer = AttendanceSerializer(attendances, many=True)
 #     return Response(serializer.data)
+
+@api_view(['POST'])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    print(serializer)
+    if serializer.is_valid():
+        serializer.save()
+        return Response("user created")
+    else:
+        return Response("not created")
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
